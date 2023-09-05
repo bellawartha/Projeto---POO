f57efcd8325a2e25d404bffb485f94da8aa0c827
Isabella Wartha <91399248+bellawartha@users.noreply.github.com>
2023-09-05 15:18:05 -0300

    update project

--- src/main.ts
+++ src/main.ts
@@ -5,74 +5,53 @@
 // Implemente interfaces para permitir que os visitantes naveguem pelas exposições e aprendam mais sobre os itens exibidos.
 // Utilize polimorfismo para exibir informações detalhadas sobre cada item da exposição.
 
-abstract class MuseumObject {
-    name: string;
-    id: number;
-};
 
-interface InfosObject {
-    infosObject () : void;
-};
+// MUSEU
 
-interface Exhibition {
-    type:string;
-    name:string;
-
-    exhibitionSetup ():void;
-    exhibitionAssembly ():void;
-};
-
-class HistoricalObject extends MuseumObject {
-    type: string;
-    constructor (name:string, id:number, type:string) {
-        super();
-        this.type = type;
-    };
-};
-
-class ArtObject extends MuseumObject {
-    type: string;
-    constructor (name:string, id:number, type:string) {
-        super();
-        this.type = type;
-    };
-};
-
-class HistoricalExhibition implements Exhibition {
-    type:string = "historical";
-    name: string;
-
-    constructor (name:string) {
-        this.name = name;
-    };
-
-    exhibitionSetup(): void {
-        
-    };
-
-    exhibitionAssembly(): void {
-        
-    };
-};
-
-class ArtExhibition implements Exhibition {
-    type: string = "art";
-    name: string;
-
-    constructor (name:string) {
-        this.name = name;
-    }
-
-    exhibitionSetup(): void {
-        
-    };
-
-    exhibitionAssembly(): void {
-        
-    };
-};
-
-class Museum {
+class Museum implements Infos, Objects {
     exhibitions: HistoricalExhibition[] | ArtExhibition[];
     objects: HistoricalObject[] | ArtObject[];
+
+    addObject(object: HistoricalObject | ArtObject) {
+        this.objects.push(object);
+    };
+
+    removeObject() {
+
+    };
+
+    addExhibition() {
+
+    };
+
+    getInfos() {
+
+    };
 };
+
+//História
+
+let obj = new HistoricalObject('Coroa de Dom Pedro II', 1, 'Metal, pedrarias e tecido', 1841);
+let obj1 = new HistoricalObject('Cristo Redentor', 2, 'Estátua monumental', 1931);
+let obj2 = new HistoricalObject('Carta de Pero Vaz de Caminha', 3, 'Documento histórico', 1500);
+let obj3 = new HistoricalObject('Espada de Duque de Caxias', 4, 'Arma histórica', "Século XIX");
+
+// Arte
+let obra1 = new ArtObject('A Última Ceia', 3, 'Leonardo DaVinci', 1498, 'Afresco');
+let obra2 = new ArtObject('Mona Lisa', 4, 'Leonardo DaVinci', 1503, 'Óleo sobre tela');
+let obra3 = new ArtObject('A Criação de Adão', 5, 'Michelangelo', 1512, 'Afresco');
+let obra4 = new ArtObject('O Nascimento de Vênus', 6, 'Sandro Botticelli', 1486, 'Têmpera sobre tela');
+let obra5 = new ArtObject('A Escola de Atenas', 7, 'Rafael Sanzio', 1510, 'Afresco');
+
+let expoArte = new ArtExhibition('A arte renascentista');
+let expoHistoria = new HistoricalExhibition('A história do Brasil');
+
+expoArte.addObject(obra1);
+expoArte.addObject(obra2);
+expoArte.addObject(obra3);
+
+console.log (expoArte.objects)
+
+
+let museuDeArte = new Museum();
+let museuDeHistoria = new Museum();
